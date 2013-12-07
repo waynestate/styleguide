7d4a56cd0641eb9c044d9d78bfa0f34e745cb93f
Nick DeNardis <nick.denardis@gmail.com>
2013-12-06 19:30:56 -0500

    Added the wordmark widht/height.

--- wsuheader/foundation/v5/header.php
+++ wsuheader/foundation/v5/header.php
@@ -1,7 +1,7 @@
 <div class="wsuheader">
   <div class="row">
     <div class="small-12 medium-6 large-8 columns wsuwordmark">
-      <a href="http://wayne.edu/"><img alt="Wayne State University" src="//assets.wayne.edu/headers/main/foundation/v5/waynestate-wordmark.png"/></a>
+      <a href="http://wayne.edu/"><img alt="Wayne State University" src="//assets.wayne.edu/headers/main/foundation/v5/waynestate-wordmark.png" width="350" height="34" /></a>
     </div>
 
     <div class="small-12 medium-6 large-4 columns wsusearch hide-for-print">
