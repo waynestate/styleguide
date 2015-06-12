3cc41aaff69408391ec604bf31365015ff73e14d
Robert Vrabel <rvrabel@gmail.com>
2015-06-12 10:22:49 -0400

    Removed icon dependency for the search. Added new media queries file Formatting.

--- wsuheader/foundation/v5/header.php
+++ wsuheader/foundation/v5/header.php
@@ -1,30 +1,33 @@
 <div class="wsuheader">
-  <div class="row">
-    <div class="small-12 medium-6 large-8 columns wsuwordmark">
-      <a href="http://wayne.edu/"><img alt="Wayne State University" src="//assets.wayne.edu/headers/main/foundation/v5/waynestate-wordmark.png" width="350" height="34" /></a>
-    </div>
-
-    <div class="small-12 medium-6 large-4 columns wsusearch hide-for-print">
-      <nav>
-        <ul>
-          <li><a href="http://pipeline.wayne.edu/">Pipeline</a></li>
-          <li><a href="http://wayne.edu/directory/">Directory</a></li>
-          <li><a href="http://wayne.edu/contact/">Contact</a></li>
-          <li class="aimhigher"><a href="http://wayne.edu/aimhigher/">Aim Higher</a></li>
-        </ul>
-      </nav>
-
-      <form method="get" action="http://wayne.edu/search/">
-        <div class="row collapse">
-          <div class="small-10 columns">
-            <label for="q">Search:</label>
-            <input name="q" id="q" placeholder="Search..." type="text" />
-          </div>
-          <div class="small-2 columns">
-            <button type="submit" class="button postfix"><span class="fi-magnifying-glass">Search</span></button>
-          </div>
+    <div class="row">
+        <div class="small-12 medium-12 large-6 xlarge-8 columns wsuwordmark">
+            <a href="http://wayne.edu/"><img alt="Wayne State University"
+                                             src="/_resources/images/waynestate-wordmark.png"
+                                             width="350" height="34"/></a>
         </div>
-      </form>
+
+        <div class="small-12 medium-12 large-6 xlarge-4 columns wsusearch hide-for-print">
+            <nav>
+                <ul>
+                    <li><a href="http://pipeline.wayne.edu/">Pipeline</a></li>
+                    <li><a href="http://wayne.edu/directory/">Directory</a></li>
+                    <li><a href="http://wayne.edu/contact/">Contact</a></li>
+                    <li class="aimhigher"><a href="http://wayne.edu/aimhigher/">Aim Higher</a></li>
+                </ul>
+            </nav>
+
+            <form method="get" action="http://wayne.edu/search/">
+                <div class="row collapse">
+                    <div class="small-10 medium-10 columns">
+                        <label for="q">Search:</label>
+                        <input name="q" id="q" placeholder="Search..." type="text"/>
+                    </div>
+                    <div class="small-2 medium-2 columns">
+                        <button type="submit" class="button postfix"><span class="search-icon"><span>Search</span></span>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
 </div>
