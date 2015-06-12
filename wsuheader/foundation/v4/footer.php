222a71a045ccc4bb132c805049f49c33b7ad79bc
Robert Vrabel <rvrabel@gmail.com>
2015-06-12 11:01:14 -0400

    Removing these since they aren't used

--- wsuheader/foundation/v4/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="wsufooter-wrapper">
-	<div class="row wsufooter">
-		<div class="small-12 large-12 columns">
-			<a class="left" href="http://wayne.edu/policies/">Privacy and University Policies</a>
-			<p class="right"><a href="http://wayne.edu/">Wayne State University</a> &copy; <?php echo date("Y"); ?></p>
-		</div>
-	</div>
-</div>
