45fdaffd33668a5881e9a4be51cf8db9f50cfe95
Robert Vrabel <rvrabel@gmail.com>
2016-08-10 14:12:47 -0400

    Revert back to PNG since SVG isn't ready

--- src/blade/header.blade.php
+++ src/blade/header.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="small-12 medium-12 large-6 xlarge-8 columns wsuwordmark">
             <a href="http://wayne.edu/"><img alt="Wayne State University"
-                                             src="/_resources/images/waynestate-wordmark.svg"
+                                             src="/_resources/images/waynestate-wordmark.png"
                                              width="350" height="34"/></a>
         </div>
 
