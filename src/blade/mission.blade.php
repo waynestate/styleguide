60d0b96e5858f3dd4c06ff2494d61da6079fb6e5
Jenny Ingles <jennyingles@wayne.edu>
2016-12-14 15:09:30 -0500

    For some reason position absolute beats source order

--- src/blade/mission.blade.php
+++ src/blade/mission.blade.php
@@ -1,9 +1,9 @@
 <div class="wsu-mission-background">
+    <div class="wsu-mission-overlay"></div>
     <div class="row">
         <div class="small-12 wsu-mission text-center columns">
             <h2><a href="//wayne.edu/about/mission">Our mission</a></h2>
             <p><a href="//wayne.edu/about/mission">We will create and advance knowledge, prepare a diverse student body to thrive, and positively impact local and global communities.</a></p>
         </div>
     </div>
-    <div class="wsu-mission-overlay"></div>
 </div>
