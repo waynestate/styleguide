4ced5a215e27c6e1dedbeaf5a8b4b3c20b155bef
Jenny Porada <jennyingles@wayne.edu>
2017-03-16 15:12:41 -0400

    Universal header

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!Doctype>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="scss/_footer.scss">
+</head>
+<body style="margin: 0;">
+
+<?php include 'html/header.html' ?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<?php include 'html/footer.html' ?>
+
+</body>
+</html>
