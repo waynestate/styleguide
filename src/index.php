349e5b9845028f91160a35396661f58c131d05e5
Jenny Porada <jennyingles@wayne.edu>
2017-03-16 19:00:47 -0400

    Framework agnostic header/footer is go w/ print styles

--- src/index.php
+++ src/index.php
@@ -1,17 +1,25 @@
-<!Doctype>
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="scss/_footer.scss">
+<link rel="stylesheet" type="text/css" href="css/_wsuheader.css">
 </head>
 <body style="margin: 0;">
 
 <?php include 'html/header.html' ?>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+
+<div class="wsuwrap">
+
+    <h1>Test page</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae elementum neque. Nullam erat dolor, scelerisque ac purus id, bibendum tempor tellus. Proin blandit, arcu id rutrum malesuada, quam est ornare metus, ut accumsan odio arcu at turpis. Nunc ut velit ex. Proin bibendum tortor pulvinar purus malesuada vulputate. Ut accumsan bibendum massa, quis ultrices odio scelerisque ut. Sed sit amet lobortis est. Fusce suscipit non risus vitae sollicitudin. Duis eros augue, faucibus nec sem nec, congue eleifend dui. Nunc nec purus quis quam molestie ornare. Maecenas ac urna vel augue sollicitudin lobortis. Praesent ex eros, mattis id ullamcorper ut, tincidunt quis arcu.</p>
+
+    <p>Sed sed sodales sapien. In mollis et nibh in lobortis. Ut a lacus aliquet arcu hendrerit luctus. Vestibulum pretium, elit eu finibus ultrices, est felis pharetra tortor, nec fringilla ex libero nec odio. Vestibulum auctor enim id pharetra pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras lorem ex, interdum in felis vel, tincidunt gravida sapien. Aliquam eleifend, nisl sed consequat consequat, sapien nunc aliquet leo, a elementum tellus risus sed velit. Praesent accumsan lobortis augue eu varius. Nunc et pulvinar est. Aenean volutpat nibh et dapibus vehicula. Vivamus nec nibh aliquam, sodales erat id, lacinia eros. Vivamus ex ante, sodales quis purus vel, venenatis molestie magna. Donec tincidunt orci dignissim erat tristique, et feugiat lacus euismod.</p>
+
+    <p>Sed tristique feugiat felis ultrices bibendum. Quisque finibus tortor at enim fringilla tincidunt. Ut hendrerit purus quis risus convallis ornare. Donec in nunc commodo, aliquet turpis quis, pharetra ante. Curabitur imperdiet odio ante, sed vehicula metus condimentum consectetur. Etiam id nunc tristique diam tristique blandit vitae quis metus. Praesent interdum vulputate laoreet. Nullam sed ullamcorper sem, id viverra nunc. Aliquam erat volutpat. Cras enim purus, blandit ut purus ut, faucibus mattis leo. Vivamus odio augue, porta nec nisl non, aliquet consequat augue.</p>
+
+    <p>&nbsp;</p>
+</div>
+
 <?php include 'html/footer.html' ?>
 
 </body>
